bcbe2d747800eedc885912c039bb65e4ce78274b
Daniel Berihun <danielshitaye10@gmail.com>
2024-04-28 09:29:12 -0700

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,18 +18,22 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i <= size - 1; i++)
+	for (i = 0; i <= size - 2 ; i++)
 	{
 		min_index = i;
-		for (j = min_index + 1; j < size ; j++)
+		for (j = i + 1; j < size ; j++)
 		{
 			if (array[min_index] > array[j])
 				min_index = j;
 		}
 
-		temp = array[i];
-		array[i] = array[min_index];
-		array[min_index] = temp;
-		print_array(array, size);
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
 	}
+
 }
