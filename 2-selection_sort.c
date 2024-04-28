8588e40fde4469a17c56095d2fac747b9a9d68c4
Daniel Berihun <danielshitaye10@gmail.com>
2024-04-28 09:05:21 -0700

    selection sorting

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts array of integers using selection sort
+ * @array: pointer to the array
+ * @size: size of the array
+ *
+ * Return: nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int min_index = 0;
+	int temp = 0;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i <= size - 1; i++)
+	{
+		min_index = i;
+		for (j = min_index + 1; j < size ; j++)
+		{
+			if (array[min_index] > array[j])
+				min_index = j;
+		}
+
+		temp = array[i];
+		array[i] = array[min_index];
+		array[min_index] = temp;
+		print_array(array, size);
+	}
+}
