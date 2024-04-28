dc961e056faa0f883a622874aa370d5e98e24c3d
Daniel Berihun <danielshitaye10@gmail.com>
2024-04-28 04:02:11 -0700

    print after each sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,13 +26,13 @@ void bubble_sort(int *array, size_t size)
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
 					flag = 1;
+					print_array(array, size);
 				}
 			}
 
 			if (flag == 0)
 				break;
 
-			print_array(array, size);
 		}
 	}
 }
