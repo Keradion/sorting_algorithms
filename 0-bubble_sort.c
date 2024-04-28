bffd7c9f4552e800967e438768aa7263a90464fb
Daniel Berihun <danielshitaye10@gmail.com>
2024-04-28 05:19:26 -0700

    size < 2 or null

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,23 +14,24 @@ void bubble_sort(int *array, size_t size)
 	int temp = 0;
 	int flag = 0;
 
-	if (size >= 2)
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
-			for (j = 0; j < size - 1 - i; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-					flag = 1;
-					print_array(array, size);
-				}
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				flag = 1;
+				print_array(array, size);
 			}
-			if (flag == 0)
-				break;
 		}
+		if (flag == 0)
+			break;
 	}
 }
+
